e72727943ff0b89fe5e40fbbdf3207ec36c6fa6e
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2017-11-28 09:47:54 +0100

    Cleanup tests

--- src/test/java/de/tum/in/www1/exerciseapp/cucumber/CucumberTest.java
+++ src/test/java/de/tum/in/www1/exerciseapp/cucumber/CucumberTest.java
@@ -7,9 +7,11 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.springframework.test.context.ActiveProfiles;
 
+/*
 @ActiveProfiles(profiles = "jira,bamboo,bitbucket")
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "pretty", features = "src/test/features")
+*/
 public class CucumberTest  {
 
 }
